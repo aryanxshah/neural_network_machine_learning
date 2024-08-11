09802ff16e934e7857997d85be357fb60d9b02f7
Aryan Shah <119019396+aryanxshah@users.noreply.github.com>
2024-08-11 17:26:16 -0500

    Update face_mask_detection.py

--- face_mask_detection.py
+++ face_mask_detection.py
@@ -110,5 +110,66 @@ X_trained_scaled[0]
 
 #build convolutional nueral network (CNN)
 num_classes = 2
-model = keras.Sequencial()
+model = keras.Sequential()
+model.add(keras.layers.Conv2D(32, kernel_size=(3,3), activation='relu', input_shape = (128,128,3)))
+model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
+
+model.add(keras.layers.Conv2D(64, kernel_size=(3,3), activation='relu'))
+model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
+model.add(keras.layers.Flatten())
+
+model.add(keras.layers.Dense(128, activation='relu'))
+model.add(keras.layers.Dropout(0.5))
+
+model.add(keras.layers.Dense(64, activation='relu'))
+model.add(keras.layers.Dropout(0.5))
+
+model.add(keras.layers.Dense(num_classes, activation='sigmoid'))
+
+#compile and train neural network
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['acc'])
+
+history = model.fit(X_trained_scaled, Y_train, validation_split=0.1, epochs= 5)
+
+#eval model
+loss, accuracy = model.evaluate(X_test_scaled, Y_test)
+print('Test Accuracy = ', accuracy)
+
+h = history
+#plot loss value
+plt.plot(h.history['loss'], label='train loss')
+plt.plot(h.history['val_loss'], label='validation loss')
+plt.legend()
+plt.show()
+
+#plot accuracy value
+plt.plot(h.history['acc'], label='train accuracy')
+plt.plot(h.history['val_acc'], label='validation accuracy')
+plt.legend()
+plt.show()
+
+#set up predictive modeling
+input_image_path = input('Path of Predicted Image: ')
+
+input_image = cv2.imread(input_image_path)
+cv2.imshow(input_image)
+
+input_image_resized = cv2.resize(input_image, (128, 128))
+input_image_scaled = input_image_resized/255
+input_image_reshaped = np.reshape(input_image_scaled, [1,128,128,3])
+
+input_prediction = model.predict(input_image_reshaped)
+print(input_prediction)
+
+input_pred_label = np.argmax(input_prediction)
+print(input_pred_label)
+
+if input_pred_label == 1:
+    print('The person in the image is wearing a mask')
+
+if input_pred_label == 0:
+    print('The person in the image is not wearing a mask')
+
 
